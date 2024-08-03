492a1d20b35d7220dfb64a1d4bf961bf01770021
wilson0x4d <mrshaunwilson@msn.com>
2024-08-03 14:46:16 +0000

    scripts: update `run-container.ps1` to warn the user when missing `-DataDir` param

--- scripts/run-container.ps1
+++ scripts/run-container.ps1
@@ -8,11 +8,19 @@ param(
     [string]$LaunchTarget = "/bin/tmux"
 )
 if ([String]::IsNullOrWhiteSpace($DataDir)) {
+    # when not specified the datadir defaults to the
+    # directory just above the git repo. this is a
+    # personal convenience so i can access the 
+    # repo from within the container (rather than)
+    # the ephemeral snapshot pulled during container
+    # creation.
     $context = $pwd.Path
     $DataDir = [IO.Path]::GetFullPath("$context/..")
+    Write-Warning "You did not provide a -DataDir argument.`nThe data directory is defaulted to: `n$DataDir"
+    Start-Sleep -Seconds 5
 }
 if ($NonInteractive.IsPresent) {
-    podman run --rm -it -v "$DataDir`:/data" once-crunch
+    podman run --rm -it -v "$DataDir`:/data" localhost/once-crunch:latest
 } else {
-    podman run --rm -it -v "$DataDir`:/data" once-crunch "/bin/tmux"
+    podman run --rm -it -v "$DataDir`:/data" localhost/once-crunch:latest /bin/tmux
 }
