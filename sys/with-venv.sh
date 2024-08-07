a1185297ba45de05a761fd1abe613eaab26243bb
wilson0x4d <mrshaunwilson@msn.com>
2024-08-06 23:38:15 -0500

    fix: sub-shell handling

--- sys/with-venv.sh
+++ sys/with-venv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2023 Shaun Wilson
+# SPDX-FileCopyrightText: © 2024 Shaun Wilson
 # SPDX-License-Identifier: MIT
 ##
 source ~/.bashrc
 export PS1='\W:\$ '
 source .venv/bin/activate
+poetry shell
