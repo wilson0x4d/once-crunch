e3ddda2a52293963158730cd267f8fa59864bfbc
wilson0x4d <mrshaunwilson@msn.com>
2024-08-09 03:07:57 -0500

    fix(scripts): outdated target path for game files

--- scripts/download-game.ps1
+++ scripts/download-game.ps1
@@ -9,4 +9,5 @@ if ([string]::IsNullOrWhiteSpace($AccountName)) {
     Write-Error "Required 'Account Name' was not provided. Aborting."
     return
 }
-steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /data/game +login $AccountName +app_update 2139460 +quit
+steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /data/once-human +login $AccountName +app_update 2139460 +quit
+
